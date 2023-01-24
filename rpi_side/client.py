80977b9d3cd9664cbbf29bea8788a1d366220c1e
Faust <denisgr99@gmail.com>
2023-01-24 17:33:34 +0300

    Changed the fps to 30

--- rpi_side/client.py
+++ rpi_side/client.py
@@ -15,6 +15,7 @@ connection = client_socket.makefile('wb')
 try:
     camera = picamera.PiCamera()
     camera.resolution = (640, 480)
+    camera.framerate = 30
     # Start a preview and let the camera warm up for 2 seconds
     camera.start_preview()
     time.sleep(2)
