ec1cbf0e301407b13b9de9ced8b19efbc92b74eb
Mikhail <konenkovma@gmail.com>
2023-01-25 17:25:00 +0300

    Updated detector params

--- PC_side/Stream_recog.py
+++ PC_side/Stream_recog.py
@@ -20,10 +20,10 @@ print('Before trying')
 detector = apriltag.Detector(
 		families = 'tag36h11',
 		nthreads = 1,
-		quad_decimate = 1.0,
-		quad_sigma = 0.2,
+		quad_decimate = 2.0,
+		quad_sigma = 0.7,
 		refine_edges = 1,
-		decode_sharpening = 0.25,
+		decode_sharpening = 0.45,
 		debug = 0)
 try:
     while True:
@@ -42,31 +42,35 @@ try:
         # processing on it
         image_stream.seek(0)
         image = Image.open(image_stream)
-        opencvImage = cv2.cvtColor(numpy.array(image), cv2.COLOR_RGB2GRAY)
+        opencvImage = cv2.cvtColor(numpy.array(image), cv2.COLOR_RGB2BGR)
             #Aprilshit
-#        gray = cv2.cvtColor(opencvImage,cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(opencvImage,cv2.COLOR_BGR2GRAY)
         #cv2.imshow("gray", gray)
-        results = detector.detect(opencvImage, estimate_tag_pose=True, camera_params=(4.9989302084566577e+02, 5.0320386297363052e+02, 3.2668799142880744e+02, 2.3439979484610001e+02), tag_size=0.0375)
+        results = detector.detect(gray, estimate_tag_pose=True, camera_params=(4.9989302084566577e+02, 5.0320386297363052e+02, 3.2668799142880744e+02, 2.3439979484610001e+02), tag_size=0.0375)
 #        print("[INFO] {} total AprilTags detected".format(len(results)))
-#        for r in results:
-#            (ptA,ptB,ptC,ptD) = r.corners
-#            ptB = (int(ptB[0]), int(ptB[1]))
-#            ptC = (int(ptC[0]), int(ptC[1]))
-#            ptD = (int(ptD[0]), int(ptD[1]))
-#            ptA = (int(ptA[0]), int(ptA[1]))
-#            # draw the bounding box of the AprilTag detection
-#            cv2.line(opencvImage, ptA, ptB, (0, 255, 0), 2)
-#            cv2.line(opencvImage, ptB, ptC, (0										, 255, 0), 2)
-#            cv2.line(opencvImage, ptC, ptD, (0, 255, 0), 2)
-#            cv2.line(opencvImage, ptD, ptA, (0, 255, 0), 2)
+        for r in results:
+            (ptA,ptB,ptC,ptD) = r.corners
+            ptB = (int(ptB[0]), int(ptB[1]))
+            ptC = (int(ptC[0]), int(ptC[1]))
+            ptD = (int(ptD[0]), int(ptD[1]))
+            ptA = (int(ptA[0]), int(ptA[1]))
+            # draw the bounding box of the AprilTag detection
+            cv2.line(opencvImage, ptA, ptB, (0, 255, 0), 2)
+            cv2.line(opencvImage, ptB, ptC, (0, 255, 0), 2)
+            cv2.line(opencvImage, ptC, ptD, (0, 255, 0), 2)
+            cv2.line(opencvImage, ptD, ptA, (0, 255, 0), 2)
             # draw the center (x, y)-coordinates of the AprilTag
-#            (cX, cY) = (int(r.center[0]), int(r.center[1]))
-#            cv2.circle(opencvImage, (cX, cY), 5, (0, 0, 255), -1)
+            (cX, cY) = (int(r.center[0]), int(r.center[1]))
+            cv2.circle(opencvImage, (cX, cY), 5, (0, 0, 255), -1)
+            # draw the pose of the AprilTag
+            cv2.line(opencvImage, (cX, cY), (int(r.pose_R[0][0] * 50 + cX), int(r.pose_R[0][1] * 50 + cY)), (255, 0, 0), 2)
+            cv2.line(opencvImage, (cX, cY), (int(r.pose_R[1][0] * 50 + cX), int(r.pose_R[1][1] * 50 + cY)), (0, 0, 255), 2)
+            cv2.line(opencvImage, (cX, cY), (int(r.pose_R[2][0] * 50 + cX), int(r.pose_R[2][1] * 50 + cY)), (0, 255, 0), 2)
             # draw the tag family on the image
-#            id_fam = str(r.tag_id)
-#            cv2.putText(opencvImage, id_fam, (ptA[0], ptA[1] - 15),
-#            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-#            print("[INFO] tag family: {}".format(id_fam))
+            id_fam = str(r.tag_id)
+            cv2.putText(opencvImage, id_fam, (ptA[0], ptA[1] - 15),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            print("[INFO] tag family: {}".format(id_fam))
         cv2.imshow('Image', opencvImage)
         image.verify()
 #        print('Image is verified')
