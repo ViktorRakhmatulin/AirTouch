625bc941923cfb99ff0541dfff8695bc14c99a54
Faust <denisgr99@gmail.com>
2023-02-01 16:15:38 +0300

    Deblur works with image from file

--- Main_part/try_recog.py
+++ Main_part/try_recog.py
@@ -3,10 +3,9 @@ import os
 import cv2
 
 
-path_to_image = os.path.join(os.path.dirname(__file__),r'.\YorkTag\test\blur\1\10.png')
+path_to_image = os.path.join(os.path.dirname(__file__),r'YorkTag\test\blur\1\10.png')
 print(path_to_image)
 image = cv2.imread(path_to_image)
 cv2.imshow("Image",image)
 cv2.waitKey()
-print(path_to_image)
 predict.main(path_to_image)
