ab7e65d1ef9be46104650522c4ca2540b2460be3
Arcwy0 <konenkovma@gmail.com>
2023-03-30 17:52:41 +0300

    Arduino+Python Relay control with AprilTag

--- Main_part/fan_control/Ord_camera_arduino_connection.py
+++ Main_part/fan_control/Ord_camera_arduino_connection.py
@@ -19,6 +19,43 @@ camera_params = (506.19083684, 508.36108854,
                  317.93111342, 243.12403806)
 tag_size = 0.0375
 
+class ArduCon():
+    def __init__(self):
+        super().__init__()
+        self.Ports = serial_ports()
+        self.Speeds = speeds
+        self.realport = None
+    
+    def select(self):
+        print(self.Ports)
+        print(self.Speeds)
+        print("Choose port (by index):")
+        chosen_port = input()
+        self.chosen_port = self.Ports[int(chosen_port)]
+        print(f'Chosen port: {self.chosen_port}')
+        print("Choose baud rate (enter value):")
+        chosen_speed = input()
+        self.chosen_speed = int(chosen_speed)
+
+    def connect(self):
+        try:
+            self.realport = serial.Serial(self.chosen_port,self.chosen_speed)
+            print('Connected to Arduino!')
+        except Exception as e:
+            print(e)
+    def send_yeah(self):
+        if self.realport:
+            self.realport.write(b'1')
+    def send_no(self):
+        if self.realport:
+            self.realport.write(b'0')
+
+#Initializing Arduino
+arduino = ArduCon()
+arduino.select()
+arduino.connect()
+
+
 # Initializing detector
 detector = apriltag.Detector(
     families='tag36h11',
@@ -33,6 +70,7 @@ fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter('measuring_distance.mp4',fourcc,30,(640,480))
 i = 0
 cap = cv2.VideoCapture(0)
+arduino_state = 0
 try:
     while True:
         # Read the length of the image as a 32-bit unsigned int. If the
@@ -49,30 +87,31 @@ try:
 
         results = detector.detect(
             gray, estimate_tag_pose=True, camera_params=camera_params, tag_size=tag_size)
+        
 #        print("[INFO] {} total AprilTags detected".format(len(results)))
+        if (not results and arduino_state == 1):
+            arduino_state = 0
+            arduino.send_no()
         for r in results:
-            # extract the bounding box (x, y)-coordinates for the AprilTag
+                        # extract the bounding box (x, y)-coordinates for the AprilTag
             (ptA, ptB, ptC, ptD) = r.corners
             ptB = (int(ptB[0]), int(ptB[1]))
             ptC = (int(ptC[0]), int(ptC[1]))
             ptD = (int(ptD[0]), int(ptD[1]))
             ptA = (int(ptA[0]), int(ptA[1]))
-            # draw the bounding box of the AprilTag detection
-            cv2.line(opencvImage, ptA, ptB, (0, 255, 0), 2)
-            cv2.line(opencvImage, ptB, ptC, (0, 255, 0), 2)
-            cv2.line(opencvImage, ptC, ptD, (0, 255, 0), 2)
-            cv2.line(opencvImage, ptD, ptA, (0, 255, 0), 2)
-            # draw the center (x, y)-coordinates of the AprilTag
-            (cX, cY) = (int(r.center[0]), int(r.center[1]))
-            cv2.circle(opencvImage, (cX, cY), 5, (0, 0, 255), -1)
-            # extract camera parameters
+                        # extract camera parameters
             fx, fy, cx, cy = camera_params
-            # find camera matrix 
             K = numpy.array([fx, 0, cx, 0, fy, cy, 0, 0, 1]).reshape(3, 3)
             # extract rotation and translation vectors
             #rvec, _ = cv2.Rodrigues(r.pose_R)
             rvec = r.pose_R
-
+            april_distance = numpy.linalg.norm(r.pose_t)
+            if (april_distance < 0.2 and arduino_state == 0):
+                arduino_state = 1
+                arduino.send_yeah()
+            elif (april_distance > 0.2 and arduino_state == 1):
+                arduino_state = 0
+                arduino.send_no()
             dcoeffs = numpy.zeros(5)
             # find object points of the tag 
             opoints = numpy.float32([[1, 0, 0],
@@ -82,10 +121,16 @@ try:
             ipoints, _ = cv2.projectPoints(opoints, rvec, r.pose_t, K, dcoeffs)
             ipoints = numpy.round(ipoints).astype(int)
             # find distance between camera and tag
-            
-            april_distance = tag_size * fx / \
-                numpy.linalg.norm(ipoints[0] - ipoints[1])
-            distance = round(april_distance, 2)
+            cv2.putText(opencvImage,f'Distance: {april_distance:.2f}',(ptA[0]+40, ptA[1] - 65),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            # draw the bounding box of the AprilTag detection
+            cv2.line(opencvImage, ptA, ptB, (0, 255, 0), 2)
+            cv2.line(opencvImage, ptB, ptC, (0, 255, 0), 2)
+            cv2.line(opencvImage, ptC, ptD, (0, 255, 0), 2)
+            cv2.line(opencvImage, ptD, ptA, (0, 255, 0), 2)
+            # draw the center (x, y)-coordinates of the AprilTag
+            (cX, cY) = (int(r.center[0]), int(r.center[1]))
+            cv2.circle(opencvImage, (cX, cY), 5, (0, 0, 255), -1)
+            # find camera matrix 
             #cv2.putText(opencvImage, f'dist:{distance}', (ptA[0], ptA[1] - 30),
              #           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
@@ -104,13 +149,12 @@ try:
             # print the information about the tag: id, rotation, translation, center, corners, distance
             #print("[INFO] tag id: {}".format(id_fam))
             #print("Rotation: {}".format(r.pose_R))
-            print("Translation: {}".format(r.pose_t))
             #cv2.putText(opencvImage, f"{numpy.round(r.pose_R[0],2)}\n {numpy.round(r.pose_R[1],2)}\n {numpy.round(r.pose_R[2],2)}", (ptA[0]+25, ptA[1] - 50),
-                        # cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            cv2.putText(opencvImage, f"x,y,z: {numpy.round(r.pose_t[0,0],2)} {numpy.round(r.pose_t[1,0],2)} {numpy.round(r.pose_t[2,0],2)}", (ptA[0]+25, ptA[1] - 45),
-                       cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            cv2.putText(opencvImage,f'Distance: {numpy.linalg.norm(r.pose_t):.2f}',(ptA[0]+40, ptA[1] - 65),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            print(r.pose_t)
+            # cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            #print("Translation: {}".format(r.pose_t))
+            #cv2.putText(opencvImage, f"x,y,z: {numpy.round(r.pose_t[0,0],2)} {numpy.round(r.pose_t[1,0],2)} {numpy.round(r.pose_t[2,0],2)}", (ptA[0]+25, ptA[1] - 45),
+            #           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            #print(r.pose_t)
             #print("Center: {}".format(r.center))
             #print("Corners: {}".format(r.corners))
             #print("Distance: {}".format(distance))
@@ -119,18 +163,18 @@ try:
             # and send it to the client
             #rotation = r.pose_R
             #rot_quat = tf.transformations.quaternion_from_matrix(rotation)
-            rotation = r.pose_R
-            rotation = numpy.round(rotation, 3)
-            rot_quat = transforms3d.quaternions.mat2quat(rotation)
-            translation = r.pose_t
-            translation = numpy.round(translation, 3)
-            translation = translation.ravel()
-            string = str(id_fam) + ' ' + str(rot_quat[0]) + ' ' + str(rot_quat[1]) + ' ' + str(rot_quat[2]) + ' ' + str(rot_quat[3]) + ' ' + str(translation[0]) + ' ' + str(translation[1]) + ' ' + str(translation[2])
+            #rotation = r.pose_R
+            #rotation = numpy.round(rotation, 3)
+            #rot_quat = transforms3d.quaternions.mat2quat(rotation)
+            #translation = r.pose_t
+            #translation = numpy.round(translation, 3)
+            #translation = translation.ravel()
+            #string = str(id_fam) + ' ' + str(rot_quat[0]) + ' ' + str(rot_quat[1]) + ' ' + str(rot_quat[2]) + ' ' + str(rot_quat[3]) + ' ' + str(translation[0]) + ' ' + str(translation[1]) + ' ' + str(translation[2])
             # print(string)
             # print("")
             #cv2.putText(opencvImage, f"quat:{numpy.round(rot_quat,2)}", (ptA[0]+25, ptA[1] - 50),
             #                cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 2)
-            send_string = string.encode('utf-8')
+            #send_string = string.encode('utf-8')
             
         cv2.imshow('Image', opencvImage)
         #cv2.imwrite(f'.\calibration\{i}.png',opencvImage)
